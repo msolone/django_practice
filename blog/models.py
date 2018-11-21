9f262419414a90c39d48a26a4655e86ae00c8428
Michael Solone <michaelsolone@gmail.com>
2018-11-21 15:59:08 -0500

    Added Post model

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length = 140)
     body = models.TextField()
-    date = models.DateField()
+    date = models.DateTimeField()
 
     def __str__(self):
         return self.title
